7c866e41dc8531714bcdd333dc18c566afe38cae
jd-chaves <jd.chaves@uniandes.edu.co>
2018-04-02 01:23:42 -0500

    Comentario sobre idioma

--- server/main.js
+++ server/main.js
@@ -2,6 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import "../imports/api/ball"
 import "../imports/api/user"
 
+/*
+La pagina genera una violacion en aXe por no tener definido un lenguage, se sugiere agregarlo
+por ejemplo con WebApp.addHtmlAttributeHook
+*/
 Meteor.startup(() => {
   // code to run on server at startup
 });
